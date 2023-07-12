8b430de87023baf84bb10bee9a0874a188bdf22f
Your Name01~ <you@example.com>
2023-07-12 19:33:27 +0500

    navbar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,10 +1,25 @@
 <template>
 	<header class="header">
-		<nav class="header__nav">
+		<nav class="header__nav" v-if="header">
 			<button class="header__nav-lang">Ru</button>
 			<h1 class="header__nav-title">Заметки</h1>
-			<button class="header__nav-search">
+			<button class="header__nav-search" @click="header = false">
 				<img src="@/assets/images/search.svg" alt="search">
+			</button>
+		</nav>
+		<nav class="header__search" v-else>
+			<button class="header__search-back" @click="header = true, search = ''">
+				<img src="../assets/images/back.svg" alt="">
+			</button>
+			<input 
+				placeholder="Поиск" 
+				type="text" 
+				class="header__search-input"
+				autofocus
+				v-model="search"
+			>
+			<button class="header__search-close" @click="search = ''">
+				<img src="../assets/images/close.svg" alt="">
 			</button>
 		</nav>
 	</header>
@@ -12,6 +27,11 @@
 
 <script>
 export default {
-
+	data(){
+		return{
+			header: true,
+			search: '',
+		}
+	}
 }
 </script>
